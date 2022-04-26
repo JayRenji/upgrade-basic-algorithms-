db4395aae25505de3c0323861b86cb6df0c3444c
Jay <100322157+JayRenji@users.noreply.github.com>
2022-04-26 23:16:36 +0200

    "iteration#6"

---
+++ ex06/script.js
@@ -0,0 +1,21 @@
+console.log('Working ok');
+
+for(i=0;i<=10;i++){
+    console.log(i);
+}
+
+for (var j=0; j<=9; j++) {
+    if (j % 2 === 0) {
+            console.log(j + " es par");   
+    }
+   
+}
+
+for (var z=0; z<=10; z++) {
+    if (z<=9) {
+            console.log(z + "Intentando dormir");   
+    } else if (z = 10){
+        console.log(z + "Zzzz");
+    }
+   
+}
